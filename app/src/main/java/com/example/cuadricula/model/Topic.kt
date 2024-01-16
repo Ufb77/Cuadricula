9fb4ec875d7f930a024ed50494d2c9b9629f00f5
Jose <jafmgdam@gmail.com>
2024-01-16 13:04:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/cuadricula/model/Topic.kt
@@ -0,0 +1,8 @@
+package com.example.cuadricula.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(@StringRes val stringResourceId: Int,
+                 val num: Int,
+                 @DrawableRes val imageResourceId: Int)
